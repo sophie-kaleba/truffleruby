be43e5b809965cc85eca5fec7999986d9c56fb29
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-01-13 12:59:28 +0000

    Only focus on GET requests, for performance monitoring
    will reintroduce other kind of requests as part of the qualitative aspect of the paper

--- bench/phase/blog-rails-routes.rb
+++ bench/phase/blog-rails-routes.rb
@@ -14,32 +14,33 @@ class BlogRailsRoutes < Benchmarks
     require 'config/environment'
 
     @app = Rails.application
-    #possible_routes = ['/posts', '/posts.json']
-    #possible_routes.concat((1..100).map { |i| "/posts/#{i}"})
+    possible_routes = ['/posts', '/posts.json']
+    possible_routes.concat((1..100).map { |i| "/posts/#{i}"})
     
-    #visit_count = 10
-    #rng = Random.new(0x1be52551fc152997)
-    #@visiting_routes = Array.new(visit_count) { possible_routes.sample(random: rng) }
-    #@visiting_routes = ['/posts.json',  '/posts/25']
+    visit_count = 20
+    rng = Random.new(0x1be52551fc152997)
+    @visiting_routes = Array.new(visit_count) { possible_routes.sample(random: rng) }
   end
 
   def benchmark
       # The app mutates `env`, so we need to create one every time.
-      for a in 1..100 do
-          env = Rack::MockRequest::env_for("http://localhost/posts/25")
+      # visit each of the visit_count routes, through a GET request
+      @visiting_routes.each do |path|
+          env = Rack::MockRequest::env_for("http://localhost#{path}")
           response_array = @app.call(env)
-          unless response_array.first == 200 
+          unless response_array.first == 200 || 
             raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
           end
       end
 
-      for a in 1..100 do
-          env = Rack::MockRequest::env_for("http://localhost/posts", :method => "POST", :params => {post:{body: "Un body new ", title: "toto new", published: false}})
-          response_array = @app.call(env)
-          unless response_array.first == 302
-            raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
-          end
-      end
+      # will create 100 new blog posts via a POST request and add them to the database
+      # for a in 1..100 do
+      #     env = Rack::MockRequest::env_for("http://localhost/posts", :method => "POST", :params => {post:{body: "This is a blog post body", title: "This is a blog post title", published: false}})
+      #     response_array = @app.call(env)
+      #     unless response_array.first == 302
+      #       raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
+      #     end
+      # end
   end
 
   def verify_result(result)
