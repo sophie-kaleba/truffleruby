dcfafcbc2a0c811c1c677333f7e7f03e379501c0
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-02-08 13:36:02 +0000

    Update ror benchmarks

--- bench/phase/blog-rails-routes-one-route-one-request.rb
+++ bench/phase/blog-rails-routes-one-route-one-request.rb
@@ -2,7 +2,7 @@
 $LOAD_PATH.unshift "#{__dir__}/../railsbench"
 require_relative '../../tool/jt.rb'
 
-class BlogRailsRoutes < Benchmarks 
+class BlogRailsRoutesOneRouteOneRequest < Benchmarks 
 
   def initialize
     Dir.chdir("#{__dir__}/../railsbench") do
@@ -17,23 +17,26 @@ class BlogRailsRoutes < Benchmarks
     possible_routes = ['/posts', '/posts.json']
     possible_routes.concat((1..100).map { |i| "/posts/#{i}"})
     
-    visit_count = 20
-    rng = Random.new(0x1be52551fc152997)
-    @visiting_routes = Array.new(visit_count) { possible_routes.sample(random: rng) }
+    #visit_count = 20
+    #rng = Random.new(0x1be52551fc152997)
+    @visiting_routes = ['/posts/36']
+    #Array.new(visit_count) { possible_routes.sample(random: rng) }
   end
 
   def benchmark
       # The app mutates `env`, so we need to create one every time.
       # visit each of the visit_count routes, through a GET request
       @visiting_routes.each do |path|
-          env = Rack::MockRequest::env_for("http://localhost#{path}")
-          response_array = @app.call(env)
-          unless response_array.first == 200  
-            raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
+          for i in 1..200 do     
+            env = Rack::MockRequest::env_for("http://localhost#{path}")
+            response_array = @app.call(env)
+            unless response_array.first == 200  
+              raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
+            end
           end
       end
 
-      # will create 100 new blog posts via a POST request and add them to the database
+      # #will create 100 new blog posts via a POST request and add them to the database
       # for a in 1..100 do
       #     env = Rack::MockRequest::env_for("http://localhost/posts", :method => "POST", :params => {post:{body: "This is a blog post body", title: "This is a blog post title", published: false}})
       #     response_array = @app.call(env)
