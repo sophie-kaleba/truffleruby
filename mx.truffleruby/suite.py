0e10b5848be7cc4c3d587112b0c10dabb281fa0b
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-08-03 15:44:33 +0100

    update graal to updated fork

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "65ff80682c17f2a4e7c299b01e6e03c101aee1a8",
+                "version": "6952a66e5649da4ee1be3c6d549f614c1df96489",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "65ff80682c17f2a4e7c299b01e6e03c101aee1a8",
+                "version": "6952a66e5649da4ee1be3c6d549f614c1df96489",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
