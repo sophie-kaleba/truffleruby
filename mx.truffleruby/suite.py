97ffdab05ec3e6230c4187e5e10b94a228ce5993
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-10-10 18:54:06 +0100

    update to fetch cov changes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "6952a66e5649da4ee1be3c6d549f614c1df96489",
+                "version": "86922f5c8891172a045ba6e32a677ba6c66b092b",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "6952a66e5649da4ee1be3c6d549f614c1df96489",
+                "version": "86922f5c8891172a045ba6e32a677ba6c66b092b",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
