545c6cde7e18f44e89e27083a6a42c99ef32f7fc
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-10-12 13:51:58 +0100

    erubi - update standalone gems against ruby 3.0.3

---
+++ bench/erubi_rails/bundle/truffleruby/3.0.3.8/gems/nokogiri-1.13.1/ports/x86_64-linux/libxslt/1.1.34/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/3.0.3.8/gems/nokogiri-1.13.1/ports/x86_64-linux/libxslt/1.1.34/lib"
+XSLT_LIBS="-lxslt -L/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/3.0.3.8/gems/nokogiri-1.13.1/ports/x86_64-linux/libxml2/2.9.12/lib -lxml2 "
+XSLT_PRIVATE_LIBS="-lm"
+XSLT_INCLUDEDIR="-I/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/3.0.3.8/gems/nokogiri-1.13.1/ports/x86_64-linux/libxslt/1.1.34/include"
+MODULE_VERSION="xslt-1.1.34"
