b7b970d21bc85e2663780c1f8b01a167c4f04fb9
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-10-12 13:45:01 +0100

    update standalone gems against ruby 3.0.3

--- bench/railsbench/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxml2/2.9.12/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxml2/2.9.12/lib"
-XML2_LIBS="-lxml2 -lz -llzma    -lm "
-XML2_INCLUDEDIR="-I/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxml2/2.9.12/include/libxml2"
-MODULE_VERSION="xml2-2.9.12"
-
--- bench/railsbench/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxslt/1.1.34/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxslt/1.1.34/lib"
-XSLT_LIBS="-lxslt -L/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxml2/2.9.12/lib -lxml2 "
-XSLT_PRIVATE_LIBS="-lm"
-XSLT_INCLUDEDIR="-I/home/sopi/Documents/Side_projects/truffleruby/bench/erubi_rails/bundle/truffleruby/2.7.3.8/gems/nokogiri-1.12.5/ports/x86_64-unknown-linux-gnu/libxslt/1.1.34/include"
-MODULE_VERSION="xslt-1.1.34"
