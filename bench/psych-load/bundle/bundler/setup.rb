5418d4ad88c0fd442a48410de773b457f01e6c0b
Sophie Kaleba <sophie.kaleba@gmail.com>
2022-11-25 10:58:43 +0000

    Update PsychLoad to ruby 3.1.2

--- bench/psych-load/bundle/bundler/setup.rb
+++ bench/psych-load/bundle/bundler/setup.rb
@@ -2,15 +2,6 @@ require 'rbconfig'
 ruby_engine = RUBY_ENGINE
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
-kernel = (class << ::Kernel; self; end)
-[kernel, ::Kernel].each do |k|
-  if k.private_method_defined?(:gem_original_require)
-    private_require = k.private_method_defined?(:require)
-    k.send(:remove_method, :require)
-    k.send(:define_method, :require, k.instance_method(:gem_original_require))
-    k.send(:private, :require) if private_require
-  end
-end
-$:.unshift File.expand_path("#{path}/")
-$:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-linux/3.0.2.10/psych-4.0.1")
-$:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/psych-4.0.1/lib")
+$:.unshift "#{path}/"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-linux/3.1.2.3/psych-4.0.1"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/psych-4.0.1/lib"
