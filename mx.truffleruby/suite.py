456f071be14a43e6af431bdf79cf9f8621b3162a
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-01-13 18:01:01 +0000

    Point to modified graal with custom polymorphicSpecialize startegy

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "d74e2a0fc574551b0ed4826c9b66ef75ebf529b8",
+                "version": "be49b8eb6ba3fb1ceb78d5fdff4247d32bfecfe8",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "d74e2a0fc574551b0ed4826c9b66ef75ebf529b8",
+                "version": "be49b8eb6ba3fb1ceb78d5fdff4247d32bfecfe8",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
