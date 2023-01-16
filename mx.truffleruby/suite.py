5a601c2d4615f5661030ad582c302e3c54d139e7
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-01-16 16:34:19 +0000

    Update the commit number after a further update of graal

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "be49b8eb6ba3fb1ceb78d5fdff4247d32bfecfe8",
+                "version": "9cd2f6cd8261a99fa433ce1176b4e604dd8d298f",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "be49b8eb6ba3fb1ceb78d5fdff4247d32bfecfe8",
+                "version": "9cd2f6cd8261a99fa433ce1176b4e604dd8d298f",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
