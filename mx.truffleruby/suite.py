d62372cddd686b517163849b669be184ae72fdc7
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-02-16 14:49:08 +0000

    Point to latest fork commit ~ set the cache status in report polymorphism

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "eed36e2d4670fe41eb9c70cde8be577bdea1a760", # with no extra fields on call nodes
+                "version": "e2702a76f1d53f3409ebf306b84ac4258a3f0867", # with no extra fields on call nodes
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "eed36e2d4670fe41eb9c70cde8be577bdea1a760", # with no extra fields on call nodes
+                "version": "e2702a76f1d53f3409ebf306b84ac4258a3f0867", # with no extra fields on call nodes
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
