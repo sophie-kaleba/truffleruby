d75ef71091234337a3734b5bcd6cab581012d9bf
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-04-10 18:00:01 +0100

    update graal with alternate way to count nodes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "90123adc1712c12c95cf2e63ea3a6c5a6ce70091",
+                "version": "f74b0eb1e37c054b447e72b818a217936c1877f4",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "90123adc1712c12c95cf2e63ea3a6c5a6ce70091",
+                "version": "f74b0eb1e37c054b447e72b818a217936c1877f4",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
