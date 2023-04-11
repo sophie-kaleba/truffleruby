d12f25f91ab5ccba20f4a1c19ec2e43dab15c913
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-04-12 00:33:11 +0100

    update graal (use thread safe code counting)

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "f74b0eb1e37c054b447e72b818a217936c1877f4",
+                "version": "d849603f1b28e5011220f6c9ee05e494c4528e84",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "f74b0eb1e37c054b447e72b818a217936c1877f4",
+                "version": "d849603f1b28e5011220f6c9ee05e494c4528e84",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
