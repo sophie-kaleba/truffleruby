a853a1b4e788fd9d10d635820923081a1d478b49
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-08-15 13:19:31 +0100

    Defer the dispatch trigger by 1 caller

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "6eebd950ea5942df66c5de884990744216d69ab2",
+                "version": "e4de98f966924283e2da6d7d0abf91c0eb12e495",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "6eebd950ea5942df66c5de884990744216d69ab2",
+                "version": "e4de98f966924283e2da6d7d0abf91c0eb12e495",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
