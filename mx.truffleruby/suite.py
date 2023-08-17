320ca30a3b2216c63db8f9485d9c9f81b4a956cb
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-08-17 12:27:59 +0100

    Update statistics and trigger on first node

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "1f3be020e8a7457accf18744e2019b02f5433e5e",
+                "version": "b81211d39eb6e3b51616f1685c46517b54c82df3",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "1f3be020e8a7457accf18744e2019b02f5433e5e",
+                "version": "b81211d39eb6e3b51616f1685c46517b54c82df3",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
