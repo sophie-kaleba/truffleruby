97e774d73ff837daf62ddbbe5196e38ad2d1cf9e
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-08-17 12:15:10 +0100

    account for 10 metrics instead of 8

--- splitting_stats_adapter.py
+++ splitting_stats_adapter.py
@@ -7,7 +7,7 @@ from rebench.model.measurement import Measurement
 class SplittingStatsAdapter(RebenchLogAdapter):
 
     split_stat_file = "splitting_statistics.log"
-    num_split_features = 8
+    num_split_features = 10
 
     def __init__(self, include_faulty, executor):
         super(SplittingStatsAdapter, self).__init__(include_faulty, executor)
