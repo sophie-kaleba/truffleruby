b3fee1749ffb58d6fc32fed991369f6142b1690f
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-08-30 16:48:32 +0100

    update against last behaviour/handle-mispredicts fork

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "d88f55e265b7af02bcd5b045378bdc3e2b39bee1",
+                "version": "36ac2e6fc606ecc8d2520178965cc1631dc23c32",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "d88f55e265b7af02bcd5b045378bdc3e2b39bee1",
+                "version": "36ac2e6fc606ecc8d2520178965cc1631dc23c32",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
