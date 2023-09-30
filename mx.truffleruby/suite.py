5d063be17ec6537b7b2202eeb1a7b4b097a01dfa
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-09-30 17:07:29 +0100

    Try the alternative misprediction approach

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "5c9c81176b5f41836d313328ca955323592ba9f1",
+                "version": "42ffc2408ec1bf9d45389d5293bba495ed51b8e7",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "5c9c81176b5f41836d313328ca955323592ba9f1",
+                "version": "42ffc2408ec1bf9d45389d5293bba495ed51b8e7",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
