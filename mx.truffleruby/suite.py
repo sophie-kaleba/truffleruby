844c396cf1c0512d06021bae0d71fc70c9d3bce1
Sophie Kaleba <sophie.kaleba@gmail.com>
2024-02-19 11:53:38 +0000

    Use alternative misprediction handling (now the default one)

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "39a555c93a2de919f094aa6417a7c018417b1348",
+                "version": "42ffc2408ec1bf9d45389d5293bba495ed51b8e7",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "39a555c93a2de919f094aa6417a7c018417b1348",
+                "version": "42ffc2408ec1bf9d45389d5293bba495ed51b8e7",
                 "urls": [
                     {"url": "https://github.com/sophie-kaleba/truffle.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
